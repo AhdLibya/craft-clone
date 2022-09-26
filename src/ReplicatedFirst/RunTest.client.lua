9eeb6442433084d030f5bdd6c291add8de5f11be
Ahd libya <97837394+AhdLibya@users.noreply.github.com>
2022-09-26 14:35:14 +0200

    chunk add

--- src/ReplicatedFirst/RunTest.client.lua
+++ src/ReplicatedFirst/RunTest.client.lua
@@ -1,8 +1,8 @@
 local Chunk = require (game:GetService('ReplicatedStorage'):WaitForChild("classes"):WaitForChild("Chunk"))
 
-local chunka = Chunk.new( 0 , 1)
-local chunkb = Chunk.new( 0 , 2)
-local chunkc = Chunk.new( 0 , 3)
-local chunkd = Chunk.new( 1 , 1)
-local chunkf = Chunk.new( 1 , 2)
+Chunk.new( 0 , 1)
+Chunk.new( 0 , 2)
+Chunk.new( 0 , 3)
+Chunk.new( 1 , 1)
+Chunk.new( 1 , 2)
 
